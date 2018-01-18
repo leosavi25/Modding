e992fbe919b51dec47822facf32e29761ce962c8
leosavi25 <35539737+leosavi25@users.noreply.github.com>
2018-01-18 15:56:26 +0100

    Update ItemInit.java

--- src/main/java/com/leosavi25/mod/init/ItemInit.java
+++ src/main/java/com/leosavi25/mod/init/ItemInit.java
@@ -8,6 +8,9 @@ import com.google.common.collect.Sets;
 import com.leosavi25.mod.objects.ItemBase;
 import com.leosavi25.mod.objects.tools.ToolAxe;
 import com.leosavi25.mod.objects.tools.ToolHoe;
+import com.leosavi25.mod.objects.tools.ToolPickaxe;
+import com.leosavi25.mod.objects.tools.ToolShovel;
+import com.leosavi25.mod.objects.tools.ToolSword;
 
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
@@ -22,15 +25,27 @@ public class ItemInit {
 
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
-	public static final ToolMaterial TOOL_COPPER = EnumHelper.addToolMaterial("tool_copper", 2, 180, 5.0f, 0.0f, 5);
+	public static ToolMaterial TOOL_COPPER;
 	
-	public static final Item COPPER_INGOT = new ItemBase("copper_ingot", CreativeTabs.MATERIALS);
-	public static final Item COPPER_NUGGET = new ItemBase("copper_nugget", CreativeTabs.MATERIALS);
+	public static Item COPPER_INGOT;
+	public static Item COPPER_NUGGET;
 
-	public static final Item COPPER_HOE = new ToolHoe("copper_hoe", TOOL_COPPER, CreativeTabs.TOOLS);
-	public static final Item COPPER_AXE = new ToolAxe("copper_axe", TOOL_COPPER, 10, -3.1f, CreativeTabs.TOOLS);
-	public static final Item COPPER_SHOVEL = new ToolHoe("copper_shovel", TOOL_COPPER, CreativeTabs.TOOLS);
-	public static final Item COPPER_SWORD = new ToolHoe("copper_sword", TOOL_COPPER, CreativeTabs.COMBAT);
-	public static final Item COPPER_PICKAXE = new ToolHoe("copper_pickaxe", TOOL_COPPER, CreativeTabs.TOOLS);
+	public static Item COPPER_HOE;
+	public static Item COPPER_AXE;
+	public static Item COPPER_SHOVEL;
+	public static Item COPPER_SWORD;
+	public static Item COPPER_PICKAXE;
+	
+	public static void init(){
+		TOOL_COPPER = EnumHelper.addToolMaterial("tool_copper", 2, 180, 5.0f, 0.0f, 5);
+		COPPER_INGOT = new ItemBase("copper_ingot", CreativeTabs.MATERIALS);
+		COPPER_NUGGET = new ItemBase("copper_nugget", CreativeTabs.MATERIALS);
+		COPPER_HOE = new ToolHoe("copper_hoe", TOOL_COPPER, CreativeTabs.TOOLS);
+		COPPER_AXE = new ToolAxe("copper_axe", TOOL_COPPER, 10, -3.1f, CreativeTabs.TOOLS);
+		COPPER_SWORD = new ToolSword("copper_sword", TOOL_COPPER, CreativeTabs.TOOLS);
+		COPPER_SHOVEL = new ToolShovel("copper_shovel", TOOL_COPPER, CreativeTabs.TOOLS);
+		COPPER_PICKAXE = new ToolPickaxe("copper_pickaxe", TOOL_COPPER, CreativeTabs.TOOLS);
+		
+	}
 
 }
